408ae8860b0262dc265263f877ecb719e6bad99d
Ravi Sendhav <ravindra.sendhav@innoeye.com>
2020-09-09 15:54:18 +0530

    Created repo

---
+++ CustomModalStatus/CustomModalStatus.h
@@ -0,0 +1,19 @@
+//
+//  CustomModalStatus.h
+//  CustomModalStatus
+//
+//  Created by apple on 09/09/20.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomModalStatus.
+FOUNDATION_EXPORT double CustomModalStatusVersionNumber;
+
+//! Project version string for CustomModalStatus.
+FOUNDATION_EXPORT const unsigned char CustomModalStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomModalStatus/PublicHeader.h>
+
+
---
+++ CustomModalStatusTests/CustomModalStatusTests.swift
@@ -0,0 +1,34 @@
+//
+//  CustomModalStatusTests.swift
+//  CustomModalStatusTests
+//
+//  Created by apple on 09/09/20.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+import XCTest
+@testable import CustomModalStatus
+
+class CustomModalStatusTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
